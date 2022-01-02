fb786714e880d8703157334a4295da2dadf2e34c
unknown <christian.llumiquinga@epn.edu.ec>
2022-01-02 13:49:25 -0500

    trabajo en clase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,6 +7,14 @@ export const environment = {
     url: 'https://api.themoviedb.org/3',
     apiKey: '27b7a3e5d9594f1e18a0ac5668753fa3',
     imgPath: 'https://image.tmdb.org/t/p'
+     /*firebaseConfig : {
+        apiKey: "AIzaSyA4LOnmC3FP2MmX5si7_qi514feV3iCJq0",
+        authDomain: "movies-nbcll.firebaseapp.com",
+        projectId: "movies-nbcll",
+        storageBucket: "movies-nbcll.appspot.com",
+        messagingSenderId: "876385684562",
+        appId: "1:876385684562:web:1e4a850950b3e5be24e38b"
+      }*/
 };
 
 /*
